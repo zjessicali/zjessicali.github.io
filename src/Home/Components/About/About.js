cf02e1ffde41f111c72f0116f7929cc293e665f1
zjessica <z.jessicali2001@gmail.com>
2024-02-28 21:22:13 -0800

    about mobile done

--- src/Home/Components/About/About.js
+++ src/Home/Components/About/About.js
@@ -35,19 +35,19 @@ function About() {
   return (
     <div className=" w-full border-t-2 border-porple bg-white">
       <div className="grid grid-cols-2 md:grid-cols-4 gap-0 md:border-r-2 border-r-white">
-        <div className="border-b-2 md:border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
+        <div className="border-b-2 border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
           <p className="font-Manrope md:text-lg lg:text-2xl m-5 md:m-4 font-medium">
             Open to work!
           </p>
         </div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
+        <div className="border-b-2  border-b-porple border-r-2 border-white md:border-r-2 md:border-porple w-full aspect-square flex flex-col justify-end">
           <p className="font-Manrope md:text-lg lg:text-2xl m-5 md:m-4 font-medium">
             Based in Vancouver BC
           </p>
         </div>
-        <div className="border-b-2 md:border-r-2 border-porple aspect-square w-full"></div>
+        <div className="border-b-2 border-r-2 border-porple aspect-square w-full"></div>
         <div
-          className="border-b-2  border-b-porple border-r-2 border-white w-full aspect-square flex flex-col justify-end"
+          className="border-b-2 border-b-porple border-r-2 border-white w-full aspect-square flex flex-col justify-end"
           onMouseOver={() => handleOnHover(0)}
           onMouseOut={() => handleOutHover(0)}
         >
@@ -56,8 +56,7 @@ function About() {
               hoveredItems[0] ? "hidden" : ""
             }`}
           >
-            jessica <br></br>
-            zhen li :)
+            jessica zhen li :)
           </p>
           <img
             alt="It's me!"
@@ -67,36 +66,39 @@ function About() {
             }`}
           ></img>
         </div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
-          <p className="font-Manrope md:mx-4 md:my-2 md:leading-none lg:leading-none">
+        <div className="border-b-2 border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
+          <p className="font-Manrope hidden md:block text-sm m-5 mb-0 md:text-base md:mx-4 md:my-2 md:leading-none lg:leading-none">
             Simon Fraser University's (2019 - present):
           </p>
-          <p className="font-Manrope md:text-lg lg:text-2xl md:mx-4 font-medium md:leading-none">
+          <p className="md:hidden font-Manrope text-sm m-5 mb-1 ">
+            SFU's (2019 - present):
+          </p>
+          <p className="font-Manrope mx-5 mb-2 leading-tight md:text-lg lg:text-2xl md:mx-4 font-medium md:leading-none">
             School of Computing Science
           </p>
-          <p className="font-Manrope md:text-lg lg:text-2xl m-5 md:m-4 md:mt-2 font-medium md:leading-none">
+          <p className="font-Manrope leading-tight md:text-lg lg:text-2xl m-5 mt-0 md:m-4 md:mt-2 font-medium md:leading-none">
             School of Interactive Arts and Technology
           </p>
         </div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full"></div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full aspect-square flex flex-col justify-end">
-          <p className="font-Manrope md:text-lg lg:text-2xl md:mx-4 font-medium md:leading-none">
+        <div className="border-b-2 border-r-2 border-porple w-full hidden md:block"></div>
+        <div className="border-b-2  border-b-porple border-r-2 border-white md:border-r-2 md:border-porple w-full aspect-square flex flex-col justify-end">
+          <p className="font-Manrope mx-5 md:text-lg lg:text-2xl md:mx-4 font-medium md:leading-none">
             user experience
           </p>
-          <p className="font-Manrope md:text-lg lg:text-2xl md:mx-4 md:mt-2 font-medium md:leading-none">
+          <p className="font-Manrope mx-5 md:text-lg lg:text-2xl md:mx-4 md:mt-2 font-medium md:leading-none">
             ui/visual design
           </p>
-          <p className="font-Manrope md:text-lg lg:text-2xl md:mx-4 md:mt-2 font-medium md:leading-none">
+          <p className="font-Manrope mx-5 md:text-lg lg:text-2xl md:mx-4 md:mt-2 font-medium md:leading-none">
             interaction design
           </p>
-          <p className="font-Manrope md:text-lg lg:text-2xl m-5 md:m-4 md:mt-2 font-medium md:leading-none">
+          <p className="font-Manrope md:text-lg lg:text-2xl m-5 mt-0 md:m-4 md:mt-2 font-medium md:leading-none">
             prototyping
           </p>
         </div>
-        <div className="border-b-2  border-porple w-full"></div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full"></div>
+        <div className="border-b-2  border-porple w-full hidden md:block"></div>
+        <div className="border-b-2 border-r-2 border-porple w-full hidden md:block"></div>
         <div
-          className="border-b-2 md:border-r-2 border-porple w-full aspect-square flex flex-col justify-end"
+          className="border-b-2 border-r-2 border-porple w-full aspect-square flex flex-col justify-end"
           onMouseEnter={() => handleOnHover(1)}
           onMouseLeave={() => handleOutHover(1)}
         >
@@ -115,7 +117,7 @@ function About() {
             }`}
           ></img>
         </div>
-        <div className="border-b-2 md:border-r-2 border-porple w-full"></div>
+        <div className="border-b-2 border-r-2 border-porple w-full hidden md:block"></div>
         <div className="border-b-2 border-porple w-full aspect-square flex flex-col justify-end">
           <p className="font-Manrope  m-5 md:m-4 font-medium">
             portfolio designed and coded by yours truly
@@ -123,6 +125,7 @@ function About() {
         </div>
       </div>
       <div className="z-50 hidden md:flex bottom-0 bg-white  px-6 w-full h-12 "></div>
+      <div className="md:hidden h-12"></div>
     </div>
   );
 }
