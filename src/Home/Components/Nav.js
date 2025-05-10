c6f7195e21bdde0d525d6d170be001d06b900f3b
zjessica <z.jessicali2001@gmail.com>
2025-05-09 23:29:12 -0700

    nav update desktop

--- src/Home/Components/Nav.js
+++ src/Home/Components/Nav.js
@@ -5,10 +5,8 @@ const Nav = () => {
     <>
       <div className="z-50 md:hidden fixed bottom-0 flex justify-between px-6 w-full h-12 items-center border-t-2 border-porple bg-white lg:text-base md:text-sm ">
         <Link to="/" className="font-Manrope md:hidden">
-          jessica ✧
+          ✧ jessica
         </Link>
-        {/* <h6 className="font-Manrope">jessica ✧</h6> */}
-        {/* <h6 className="font-Manrope">about</h6> */}
         {/* JAN 2025 UPDATE: UNLINKED BC I HATE IT AND WANNA UPDATE IT LATER */}
         {/* <Link to="about" className="font-Manrope md:hidden">
           about
@@ -16,11 +14,18 @@ const Nav = () => {
       </div>
       <div className="hidden md:flex bg-pinkie z-50 justify-between md:px-10 lg:px-20 py-8 w-full h-12 items-center">
         <Link to="/" className="font-Manrope  animate-fadeUp">
-          jessica ✧
+          ✧ jessica
         </Link>
-        <h6 className="font-Manrope opacity-50 animate-fadeUp">
-          seeking for summer 2025
-        </h6>
+        <div className="opacity-50 animate-fadeUp flex">
+          <h1 className="font-Manrope px-4">
+            <a href="https://www.linkedin.com/in/jessica-li-029281201/">
+              linkedin
+            </a>
+          </h1>
+          <h1 className="font-Manrope">
+            <a href="mailto:z.jessicali2001@gmail.com">email</a>
+          </h1>
+        </div>
       </div>
       <div></div>
     </>
