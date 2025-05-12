f722ef3389cd727e72be67d825d09d8d3e23ae66
zjessica <z.jessicali2001@gmail.com>
2025-05-12 13:20:40 -0700

    reflection added

---
+++ src/Home/Components/SojuEvents/Reflection.js
@@ -0,0 +1,47 @@
+function Reflection() {
+  return (
+    <section className="bg-white md:grid md:grid-cols-4 md:gap-5 lg:mx-20 md:mx-10 mx-6 pb-6 md:pb-10 lg:pb-20">
+      <section className="mb-4">
+        <p
+          className="font-Manrope lg:text-lg text-grey 
+    uppercase border border-grey text-center w-5/6 rounded-full lg:py-[2px]"
+        >
+          Reflection
+        </p>
+      </section>
+      <div className="md:col-span-2">
+        <p className="font-Manrope mt-5 text-porple md:text-lg">
+          Through this project, I enjoyed the process of seeing what new users
+          think about the Soju Events website, the things they care about more
+          as a user compared to what I might car about as a designer. We tried
+          our best to analyze the large variety of observations we collected to
+          transform them into concrete problems with solutions for our client to
+          implement within the scope of the project. Of course, there are a lot
+          more problems we can conver more intensively, for example, the
+          distracting visual inconsistnsies,which we recommended developing a
+          style guide.
+        </p>
+        <p className="font-Manrope mt-5 text-porple md:text-lg">
+          Through this project, I really enjoyed gaining insight into how new
+          users perceive the Soju Events website, especially noticing how their
+          priorities as users differ from mine as a designer. We carefully
+          analyzed a wide range of observations and worked to translate them
+          into concrete problems paired with feasible solutions for our client
+          to implement within the project scope. Naturally, there are still many
+          areas that could benefit from deeper attention, for example, the
+          distracting visual inconsistencies, which led us to recommend the
+          creation of a style guide. Given more time and resources, I would love
+          to take on a full redesign of the website.
+        </p>
+        <p className="font-Manrope mt-5 text-porple md:text-lg">
+          The client was enthusiastic about our report, sharing that they had
+          been seeking this kind of usability insight for years but didn’t know
+          where to begin. Shortly after, they began implementing several of our
+          recommendations into their website.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Reflection;
