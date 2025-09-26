1c1c6b4f6a294ec0f0160779238c338d972479a8
zjessica <z.jessicali2001@gmail.com>
2025-09-25 18:44:58 -0700

    hover images

--- src/Home/Components/Projects.js
+++ src/Home/Components/Projects.js
@@ -4,35 +4,47 @@ import { useState, useEffect } from "react";
 
 function Projects() {
   const projects = DATA.projects;
+
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [hoveredItem, setHoveredItem] = useState(null);
+
   return (
-    <div className="w-full">
-      <div className="w-full grid grid-cols-2 gap-10 pb-5">
+    <div
+      className="w-full relative"
+      onMouseMove={(e) => setPos({ x: e.clientX, y: e.clientY })}
+    >
+      <div className="w-full grid grid-cols-2 gap-10 pb-5 z-40 font-DM">
         {projects.map((item, i) => {
           return (
-            <Link to={item.linkpath} className="h-40 md:h-60 lg:h-80">
-              <h1 className="font-DM">
+            <Link
+              to={item.linkpath}
+              className="h-40 md:h-60 lg:h-80"
+              onMouseEnter={() => setHoveredItem(item)}
+              onMouseLeave={() => setHoveredItem(null)}
+            >
+              <h1 className="font-DM pb-1">
                 {item.title}&nbsp;
                 <span className="opacity-60">—&nbsp;{item.tags}</span>
               </h1>
-              <h2 className="opacity-60">{item.context}</h2>
+              <h2 className="opacity-60 pb-4">{item.context}</h2>
+              <p className="w-2/3">{item.blurb}</p>
             </Link>
           );
         })}
       </div>
+      {hoveredItem && (
+        <img
+          src={hoveredItem.imgpath}
+          alt={hoveredItem.title}
+          className="w-[30vw] h-auto fixed pointer-events-none rounded z-0"
+          style={{
+            top: pos.y + 36, // offset from cursor
+            left: pos.x + 16,
+          }}
+        />
+      )}
     </div>
   );
 }
 
 export default Projects;
-
-// {projects.map((item, i) => (
-//   <div>
-//     <div className="border-b-2 border-porple">
-//       <img alt="lala" src={item.imgpath}></img>
-//     </div>
-//     <div className="border-b-2 border-porple text-left px-6 py-5">
-//       <h2 className="font-Manrope text-xl">{item.title}</h2>
-//       <h3 className="font-Manrope opacity-80">{item.tags}</h3>
-//     </div>
-//   </div>
-// ))}
