bc2f74c0fd97af5b8e4d7fdb70dbe9229558acae
zjessica <z.jessicali2001@gmail.com>
2025-09-25 21:12:07 -0700

    fixed some hover

--- src/Home/Components/Nav.js
+++ src/Home/Components/Nav.js
@@ -4,11 +4,16 @@ const Nav = () => {
   return (
     <>
       <div className=" text-grey grid grid-cols-4 gap-10 p-5 w-full text-sm opacity-60">
-        <Link to="/" className="font-DM animate-fadeUp hover-link">
+        <Link
+          to="/"
+          className="font-DM animate-fadeUp hover-link inline-block w-fit"
+        >
           ✧ jessica
         </Link>
         <div className="animate-fadeUp flex col-span-3 gap-x-10">
-          <h1 className="font-DM hover-link">projects</h1>
+          <Link className="font-DM hover-link" to="/">
+            projects
+          </Link>
           <h1 className="font-DM hover-link">sandbox</h1>
           <h1 className="font-DM hover-link">about</h1>
         </div>
